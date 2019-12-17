8190305d1442391652518ea99be04f41c9dfa522
Mikhail Pravilov <prav.mischa@gmail.com>
2019-12-17 20:24:51 +0300

    Change path to data

--- configs.py
+++ configs.py
@@ -3,8 +3,8 @@ class Config:
         self.HIDDEN = 128
         self.BERT_MODEL = 'bert-base-multilingual-cased'
         self.LOG_DIR = '/mnt/data/QA_DL/LSTM/'
-        self.DEV_DATASET = './gdrive/My Drive/datasets_for_homeworks/dev-v1.1.json'
-        self.TRAIN_DATASET = './gdrive/My Drive/datasets_for_homeworks/train-v1.1.json'
+        self.DEV_DATASET = './data/QA_DL/dev-v1.1.json'
+        self.TRAIN_DATASET = './data/QA_DL/train-v1.1.json'
         self.MAX_TEXT_LENGTH = 256
         self.DEVICE = 'cuda'
         self.LR = 0.00005
