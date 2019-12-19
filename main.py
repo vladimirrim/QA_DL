49e3111e7d0d8383fd368bf97b8b9e8deaa80038
Mikhail Pravilov <prav.mischa@gmail.com>
2019-12-19 12:39:21 +0300

    Add best model saving.

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
     start = time.time()
     losses = []
     val_losses = []
+    best_val_loss = 10000
     print('Starting training', flush=True)
     for epoch in range(epochs):
         model.train()
@@ -58,36 +59,40 @@ if __name__ == '__main__':
             if (i + 1) % 100 == 0:
                 torch.save(model.state_dict(), config.LOG_DIR + 'bert.ckpt')
                 print(f'Model saved on {i} iteration!', flush=True)
+                end = time.time()
+                print(f'Elapsed time: {end - start}', flush=True)
+                start = end
+
+                model.eval()
+                val_loss = 0
+                cnt = 0
+                with torch.no_grad():
+                    for texts, masks, start_pos, end_pos in dev_data_loader:
+                        loss, _ = model(texts.to(device),
+                                        mask=masks.to(device),
+                                        start_positions=torch.tensor(start_pos).to(device),
+                                        end_positions=torch.tensor(end_pos).to(device))
+                        val_loss += float(loss)
+                        cnt += 1
+                val_losses.append(val_loss / cnt)
                 losses.append(train_loss / 100)
                 train_loss = 0
-                print(f'Train loss: {losses[-1]}')
-                if i % 100 == 0:
-                    torch.save(model.state_dict(), config.LOG_DIR + 'bert.ckpt')
-                    print(f'Model saved on {i} iteration!', flush=True)
-                    end = time.time()
-                    print(f'Elapsed time: {end - start}', flush=True)
-                    start = end
-
-                    model.eval()
-                    val_loss = 0
-                    cnt = 0
-                    with torch.no_grad():
-                        for texts, masks, start_pos, end_pos in dev_data_loader:
-                            loss, _ = model(texts.to(device),
-                                            mask=masks.to(device),
-                                            start_positions=torch.tensor(start_pos).to(device),
-                                            end_positions=torch.tensor(end_pos).to(device))
-                            val_loss += float(loss)
-                            cnt += 1
-                    val_losses.append(val_loss / cnt)
-                    losses.append(train_loss / 100)
-                    train_loss = 0
-                    print(f'Loss train: {losses[-1]}', flush=True)
-                    print(f'Loss dev: {val_losses[-1]}', flush=True)
-                    model.train()
+                print(f'Loss train: {losses[-1]}', flush=True)
+                print(f'Loss dev: {val_losses[-1]}', flush=True)
+                if best_val_loss > val_losses[-1]:
+                    best_val_loss = val_losses[-1]
+                    torch.save(model.state_dict(), config.LOG_DIR + 'bert_best_val.ckpt')
+                    print(f'Best model on validation saved on {i} iteration!', flush=True)
+                model.train()
 
     with open(config.LOG_DIR + 'losses.pkl', 'wb') as f:
         pickle.dump(losses, f)
     with open(config.LOG_DIR + 'val_losses.pkl', 'wb') as f:
         pickle.dump(val_losses, f)
 
+    print(f'Starting evaluation')
+    dev_dataset = get_text_question_ans_dataset(dev_data)
+    evaluator.evaluate(dev_dataset)
+    print(f'Starting evaluation on best model on validation')
+    model.load_state_dict(torch.load(config.LOG_DIR + 'bert_best_val.ckpt'))
+    evaluator.evaluate(dev_dataset)
